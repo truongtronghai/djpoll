b66a5ae368c03fd8f14744e4a2cfb258ac1de0cd
Trong Hai <truongtronghai@gmail.com>
2021-11-12 13:17:35 +0700

    starting for template

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):  # each class  will be a Table in DB
+    question_text = models.CharField(max_length=200)  # property is a column
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):  # override parent's method to return human-readable str
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,26 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
+# Each view is represented by a Python function or method
 
 
 def index(request):
-    return HttpResponse("Hello, this is index of Poll app")
+    latest_question_list = Question.objects.order_by(
+        '-pub_date')[:5]  # get 5 latest things
+    output_results = "<br> ".join(
+        q.question_text for q in latest_question_list)
+    return HttpResponse("<html><body><h1>Questions:</h1>%s</body></html>" % output_results)
+
+
+def detail(request, question_id):
+    return HttpResponse("<html><body><h1>You are looking at detail of question: %s</h1></body></html>" % question_id)
+
+
+def results(req, question_id):
+    return HttpResponse("You are looking at the results of question: %s" % question_id)
+
+
+def vote(req, question_id):
+    return HttpResponse("You are voting on question: %s" % question_id)
