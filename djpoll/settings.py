ed6b686e1fc3bd5b29a38c47658a8718377151a1
Trong Hai <truongtronghai@gmail.com>
2021-11-19 21:48:35 +0700

    add Whitenoise for serving static files

--- djpoll/settings.py
+++ djpoll/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # add Whitenoise for static files on Heroku
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djpoll.urls'
